ad52633f2731003a03b1515460a8de08e2e56499
MikeMagee80 <mikemagee79@gmail.com>
2023-05-09 11:13:32 -0500

    First Commit

---
+++ day_1_project/band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+
+element_1 = input("What's the name of the city you grew up in? ")
+
+element_2 = input("what's your pet's name? ")
+
+print("Your band name could be " + element_1 + " " + element_2)
