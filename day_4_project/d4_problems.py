25b1e73f01364ecc33e87a5d1d7daec67a756f6a
MikeMagee80 <73151543+MikeMagee80@users.noreply.github.com>
2024-01-15 12:55:39 -0600

    Update d4_problems.py

--- day_4_project/d4_problems.py
+++ day_4_project/d4_problems.py
@@ -18,6 +18,14 @@
 # else:
 #     print('Tails')
 
+# import random
+
+# random_side = random.randint(0, 1)
+# if random_side == 1:
+#     print('Heads')
+# else:
+#     print('Tails')
+
 
 # ########################################
 
