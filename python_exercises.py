c3676f7a4a25f63582e4a2170e81cc82dd1c532d
MikeMagee80 <73151543+MikeMagee80@users.noreply.github.com>
2024-06-16 01:34:01 -0500

    Began python exercises

---
+++ python_exercises.py
@@ -0,0 +1,13 @@
+# for number in range(3):
+#     print("Attempt")
+#     print("Attempt", number) 
+
+# Attempt              output is such because the loop architecture is sequential within the loop.
+# Attempt 0
+# Attempt
+# Attempt 1
+# Attempt
+# Attempt 2
+
+###############################
+
