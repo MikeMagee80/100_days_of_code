b6b7582f956a62db1581aad3ad315b231143d124
MikeMagee80 <73151543+MikeMagee80@users.noreply.github.com>
2024-08-28 08:29:07 -0500

    Update modules.py

---
+++ python_exercises/modules.py
@@ -0,0 +1,24 @@
+# Module: file that contains some python code.
+# Should contain highly-related objects (like aisles in a supermarket)
+
+# app.py and sales.py
+
+
+# # Move the calc functions from app.py to sales.py
+# def calc_tax():
+#     pass
+
+
+# def calc_shipping():
+#     pass
+
+# # in app.py
+# from sales import calc_shipping, calc_tax
+
+# calc_shipping()
+# calc_tax()
+
+# # or
+# import sales
+
+# sales.calc_shipping()
